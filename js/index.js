06baef48ae1344a7b8932624f6b3c8206f23baeb
Lina Shamoon <shamoonlina@gmail.com>
2018-05-04 17:21:48 -0700

    animations

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-$(".tab-content-one").show();
+$(".tab-content-one").show().addClass("animated").addClass("fadeIn");
 $(".tab-content-two").hide();
 $(".tab-content-three").hide();
 
 $(".tab-one").on('click', _ => {
-    $(".tab-content-one").show();
+    $(".tab-content-one").show().addClass("animated").addClass("fadeIn");
     $(".tab-content-two").hide();
     $(".tab-content-three").hide();
 })
 
 $(".tab-two").on('click', _ => {
     $(".tab-content-one").hide();
-    $(".tab-content-two").show();
+    $(".tab-content-two").show().addClass("animated").addClass("fadeIn");
     $(".tab-content-three").hide();
 })
 
 $(".tab-three").on('click', _ => {
     $(".tab-content-one").hide();
     $(".tab-content-two").hide();
-    $(".tab-content-three").show();
+    $(".tab-content-three").show().addClass("animated").addClass("fadeIn");
 })
